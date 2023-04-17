281fb2d6a484df3ca26e6dac770846160ace1a3d
Stefan Ockert <stefan.ockert@iubh-fernstudium.de>
2023-04-17 15:10:33 +0200

    menuVisibility

--- ipwa01/javascript/javascript.js
+++ ipwa01/javascript/javascript.js
@@ -1,23 +1,35 @@
 let left = "de-DE";
 
-function visibleLeft() {
+function menuVisibility() {
   if (navigator.language.valueOf(left)) {
-    let leftMenu = document.getElementById("visible-left");
-    let rightMenu = document.getElementById("visible-right");
+    visibleLeft();
+  } else {
+    visibleRight();
+  }
+}
+
+function visibleLeft() {
+  const leftMenu = document.getElementById("visible-left");
+  const rightMenu = document.getElementById("visible-right");
+  
+   {
     rightMenu.style.visibility = "hidden";
     rightMenu.style.maxWidth = "0%";
     leftMenu.style.visibility = "visible";
     leftMenu.style.maxWidth = "100%";
+    return
   }
 }
 
 function visibleRight() {
-  let leftMenu = document.getElementById("visible-left");
-  let rightMenu = document.getElementById("visible-right");
+  const leftMenu = document.getElementById("visible-left");
+  const rightMenu = document.getElementById("visible-right");
+  
   if (navigator.language != left) {
     leftMenu.style.visibility = "hidden";
     leftMenu.style.maxWidth = "0%";
     rightMenu.style.visibility = "visible";
     rightMenu.style.maxWidth = "100%";
+    return;
   }
 }
