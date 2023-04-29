bb5269eb01cd54f64dab91e060071fd3eec53a53
Stefan Ockert <stefan.ockert@iubh-fernstudium.de>
2023-04-29 12:52:43 +0200

    js und contact

--- ipwa01/javascript/javascript.js
+++ ipwa01/javascript/javascript.js
@@ -1,31 +1,48 @@
-let left = "de-DE";
+//Sprachen, die von rechts nach links gelesen werden und damit das Menü auf der rechten Seite anzeigen.
+const righToLeftLanguages = [
+  "ar",
+  "he",
+  "fa",
+  "ur",
+  "ks",
+  "ps",
+  "ug",
+  "ku",
+  "pa",
+  "sd",
+];
 
+//Funktion, die entscheidet, ob das Menü links oder rechts angezeigt wird.
 function menuVisibility() {
-  if (navigator.language.valueOf(left)) {
-    visibleLeft();
-  } else {
-    visibleRight();
+  //For-Each-Loop durch das Array
+  righToLeftLanguages.forEach(leftOrRight);
+  function leftOrRight(language) {
+    if (navigator.language.includes(language)) {
+      visibleRight();
+    } else {
+      visibleLeft();
+    }
   }
 }
 
 function visibleLeft() {
   const leftMenu = document.getElementById("visible-left");
   const rightMenu = document.getElementById("visible-right");
-  
-   {
+
+  {
     rightMenu.style.visibility = "hidden";
     rightMenu.style.maxWidth = "0%";
     leftMenu.style.visibility = "visible";
     leftMenu.style.maxWidth = "100%";
-    return
+    return;
   }
 }
 
 function visibleRight() {
   const leftMenu = document.getElementById("visible-left");
   const rightMenu = document.getElementById("visible-right");
-  
-  if (navigator.language != left) {
+
+  {
     leftMenu.style.visibility = "hidden";
     leftMenu.style.maxWidth = "0%";
     rightMenu.style.visibility = "visible";
